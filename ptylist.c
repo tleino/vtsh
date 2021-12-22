22a666818222fb7a1bb1dbd4002fc7158d4d454b
Tommi Leino <namhas@gmail.com>
2021-12-22 23:12:27 +0200

    Remove ^X handling from ptylist

--- ptylist.c
+++ ptylist.c
@@ -38,7 +38,6 @@ struct ptylist {
 	struct pty *ptys[100];
 	int n_ptys;
 	int i;
-	int x_on;
 	struct widget *widget;
 	struct layout *vbox;
 	struct ptylist *first;
@@ -276,14 +275,11 @@ ptylist_keypress(XKeyEvent *xkey, void *udata)
 	sym = XkbKeycodeToKeysym(DPY(ptylist->dpy), xkey->keycode, 0,
 	    (xkey->state & ShiftMask) ? 1 : 0);
 
-	if (sym == XK_s && xkey->state & ControlMask && ptylist->x_on == 1) {
+	if (sym == XK_s && xkey->state & ControlMask) {
 		pty = ptylist_find_focus(ptylist);
 		pty_save(pty);
 		warnx("saved");
-	} else if (sym == XK_x && xkey->state & ControlMask)
-		ptylist->x_on = 1;
-	else
-		ptylist->x_on = 0;
+	}
 
 	if (!(xkey->state & Mod1Mask) && sym != XK_Escape)
 		return 0;
